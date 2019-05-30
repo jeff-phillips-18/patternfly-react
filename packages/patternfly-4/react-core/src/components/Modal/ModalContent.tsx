9de1da56eddebe3cd7b5f4f19a5bd9268f6835a7
Jeffrey Phillips <jephilli@redhat.com>
2019-05-30 13:50:47 -0400

    feat(Modal): Allow custom header and footer

--- packages/patternfly-4/react-core/src/components/Modal/ModalContent.tsx
+++ packages/patternfly-4/react-core/src/components/Modal/ModalContent.tsx
@@ -6,6 +6,7 @@ import * as React from 'react';
 const FocusTrap: any = require('focus-trap-react');
 
 import styles from '@patternfly/patternfly/layouts/Bullseye/bullseye.css';
+import titleStyles from '@patternfly/patternfly/components/Title/title.css';
 import { css } from '@patternfly/react-styles';
 
 import Backdrop from '../Backdrop/Backdrop';
@@ -14,6 +15,7 @@ import { ModalBoxHeader } from './ModalBoxHeader';
 import { ModalBoxCloseButton } from './ModalBoxCloseButton';
 import { ModalBox } from './ModalBox';
 import { ModalBoxFooter } from './ModalBoxFooter';
+import {getModifier} from "../../../../react-styles/dist/js";
 
 export interface ModalContentProps {
   /** Content rendered inside the Modal. */
@@ -26,13 +28,17 @@ export interface ModalContentProps {
   isSmall?: boolean;
   /** Flag to show the modal */
   isOpen?: boolean;
-  /** Content of the Modal Header */
+  /** Complex header (more than just text), supersedes title for header content */
+  header?: React.ReactNode,
+  /** Simple text content of the Modal Header, also used for aria-label on the body */
   title: string;
-  /** Flag to show the title */
+  /** Flag to show the title (ignored for custom headers) */
   hideTitle?: boolean;
   /** Default width of the content. */
   width?: number | string;
-  /** Content of the Modal Footer */
+  /** Custom footer */
+  footer?: React.ReactNode,
+  /** Action buttons to add to the standard Modal Footer, ignored if `footer` is given */
   actions?: any,
   /** A callback for when the close button is clicked */
   onClose?: () => void;
@@ -46,8 +52,10 @@ export const ModalContent: React.FunctionComponent<ModalContentProps> = ({
   children,
   className = '',
   isOpen = false,
+  header = null,
   title,
   hideTitle = false,
+  footer = null,
   actions = [],
   onClose = () => undefined as any,
   isLarge = false,
@@ -57,12 +65,17 @@ export const ModalContent: React.FunctionComponent<ModalContentProps> = ({
   id = '',
   ...props
 }) => {
-  const modalBoxHeader = <ModalBoxHeader hideTitle={hideTitle}> {title} </ModalBoxHeader>;
-  const modalBoxFooter = actions.length > 0 && <ModalBoxFooter> {actions} </ModalBoxFooter>;
   if (!isOpen) {
     return null;
   }
 
+  const modalBoxHeader = header ?
+    <div className={css(titleStyles.title)}>{header}</div> :
+    <ModalBoxHeader hideTitle={hideTitle}> {title} </ModalBoxHeader>;
+
+  const modalBoxFooter = footer ?
+    <ModalBoxFooter>{footer}</ModalBoxFooter> :
+    actions.length > 0 && <ModalBoxFooter>{actions}</ModalBoxFooter>;
   const boxStyle = width === -1 ? {} : { width };
 
   return (
